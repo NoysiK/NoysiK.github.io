e499fff0bcc8017c3ec14e73647b8021209a9e62
NoysiK <123983098+NoysiK@users.noreply.github.com>
2023-03-07 22:50:23 -0300

    Add files via upload

---
+++ Proyecto/js/funciones.js
@@ -0,0 +1,20 @@
+var menu_visible = false;
+let menu = document.getElementById("menu-extensible");
+function MostrarOcultarMenu(){
+    if(menu_visible==false){
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+
+let links = document.querySelectorAll("menu-extensible a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
